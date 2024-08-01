b1d20e3ac73498f7052c082b30904062031aaa67
ThaiTuan <tuanvowork99@gmail.com>
2024-08-02 00:00:09 +0700

    add TODO

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
             <Header/>
             <div className="flex flex-col md:flex-row items-center justify-center">
                 <div className="text-start flex-1">
-                    //TODO Change background color without using bg-amber-200 and response mobile
+                    {/*TODO Change background color without using bg-amber-200 and response mobile*/}
                     <div className='content-center px-40 py-72 bg-amber-200'>
                         <h1 className="text-4xl font-bold mb-4 text-primary font-playfairBold">{languages.get('errorTitle')}</h1>
                         <p className="text-lg mb-8">{languages.get('errorMessage')}</p>
