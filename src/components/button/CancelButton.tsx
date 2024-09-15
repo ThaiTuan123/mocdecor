3a003a2ffce415ce95af8f42134a030cf5bc5886
ThaiTuan <tuanvowork99@gmail.com>
2024-09-16 00:39:35 +0700

    refactor cart and video and add api

---
+++ src/components/button/CancelButton.tsx
@@ -0,0 +1,34 @@
+// src/components/CancelButton.tsx
+import React from 'react';
+
+interface CancelButtonProps {
+    onClick: () => void;
+    absolute?: boolean;
+    className?: string;
+}
+
+const CancelButton: React.FC<CancelButtonProps> = ({onClick, absolute = true, className}) => {
+    return (
+        <button
+            onClick={onClick}
+            className={`${absolute ? 'absolute top-4 right-4' : ''} ${className || ''} text-black hover:text-red-500 flex items-center justify-center`}
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-8 h-8"
+                fill="none"
+                viewBox="0 0 28 28"
+                stroke="currentColor"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                />
+            </svg>
+        </button>
+    );
+};
+
+export default CancelButton;
