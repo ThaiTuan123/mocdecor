5b7864caf551eb850ab743c961b086f512082ae7
TranVanLam64 <vanlam8526@gmail.com>
2024-10-21 20:32:01 +0700

    responsive, handle delete cart

---
+++ src/recoil/atoms/cartAtom.ts
@@ -0,0 +1,64 @@
+import { atom } from 'recoil';
+
+export const cartState = atom({
+  key: 'cartState', 
+  default: [
+    {
+      id: '1',
+      title:
+        "Khung Handmade 3D, Set Nguyên liệu, Trang tríKhung Handmade 3D, Set Nguyên liệu, Trang tríKhung Handmade 3D, Set Nguyên liệu, Trang trí",
+      desc: "Xanh dương, set nguyên liệu, tự trang trí",
+      price: 175000,
+      quantity: 1,
+    },
+    {
+      id: '2',
+      title: "Khung Handmade 3D, Set Nguyên liệu, Trang trí...",
+      desc: "Xanh dương, set nguyên liệu, tự trang trí",
+      price: 175000,
+      quantity: 1,
+    },
+    {
+      id: '3',
+      title: "Khung Handmade 3D, Set Nguyên liệu, Trang trí...",
+      desc: "Xanh dương, set nguyên liệu, tự trang trí",
+      price: 175000,
+      quantity: 1,
+    },
+    {
+      id: '4',
+      title: "Khung Handmade 3D, Set Nguyên liệu, Trang trí...",
+      desc: "Xanh dương, set nguyên liệu, tự trang trí",
+      price: 175000,
+      quantity: 1,
+    },
+    {
+      id: '5',
+      title: "Khung Handmade 3D, Set Nguyên liệu, Trang trí...",
+      desc: "Xanh dương, set nguyên liệu, tự trang trí",
+      price: 175000,
+      quantity: 1,
+    },
+    {
+      id: '6',
+      title: "Khung Handmade 3D, Set Nguyên liệu, Trang trí...",
+      desc: "Xanh dương, set nguyên liệu, tự trang trí",
+      price: 175000,
+      quantity: 1,
+    },
+    {
+      id: '7',
+      title: "Khung Handmade 3D, Set Nguyên liệu, Trang trí...",
+      desc: "Xanh dương, set nguyên liệu, tự trang trí",
+      price: 175000,
+      quantity: 1,
+    },
+    {
+      id: '8',
+      title: "Khung Handmade 3D, Set Nguyên liệu, Trang trí...",
+      desc: "Xanh dương, set nguyên liệu, tự trang trí",
+      price: 175000,
+      quantity: 1,
+    },
+  ], 
+});
