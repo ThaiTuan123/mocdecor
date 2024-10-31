a0a4ec02132a9b86c588334a55f9f0084e599898
TranVanLam64 <vanlam8526@gmail.com>
2024-10-31 20:42:42 +0700

    fix 74

--- src/components/footer/FooterDiscover.tsx
+++ src/components/footer/FooterDiscover.tsx
@@ -1,5 +1,6 @@
 import languages from "@/configs/languages";
 import images from "@/configs/images";
+import Link from "next/link";
 
 const FooterDiscover = () => {
     const footerTitle = languages.get("policy.footer.title");
@@ -23,11 +24,11 @@ const FooterDiscover = () => {
             <h2 className="font-playfairBold text-2xl md:text-4lg mb-6 text-center px-6 md:px-0">
                 {footerTitle}
             </h2>
-            <div className="w-52 h-12 flex items-center justify-center border-primary border-solid border-2 cursor-pointer mb-6 md:mb-8 bg-white rounded hover:scale-105">
+            <Link href={'/products/Khung%20anh/khung-dep'} className="w-52 h-12 flex items-center justify-center border-primary border-solid border-2 cursor-pointer mb-6 md:mb-8 bg-white rounded hover:scale-105">
                 <span className="font-raleway text-primary font-bold text-lg">
                     {footerButtonText}
                 </span>
-            </div>
+            </Link>
             <div className="flex items-center gap-3.5 mb-6">
                 <div className="h-px w-11 bg-white"></div>
                 <span>{footerOrText}</span>
