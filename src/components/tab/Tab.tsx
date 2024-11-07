8794eb820a02e8f9e631b5fd4526fa74c397567f
ThaiTuan <tuanvowork99@gmail.com>
2024-11-07 20:19:16 +0700

    fix bug no #103

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -29,11 +29,11 @@ const Tab: React.FC<TabProps> = ({
         <div className={background}>
             <div className="overflow-x-auto">
                 <div
-                    className="2xl:mx-auto 2xl:container flex border-b border-stroke ml-6 md:px-36 pt-3 md:pt-7 whitespace-nowrap">
+                    className="2xl:mx-auto 2xl:container flex border-b border-stroke ml-0 lg:px-36 lg:ml-6 px-6 pt-3 md:pt-7 overflow-x-scroll whitespace-nowrap no-scrollbar">
                     {tabs.map((tab, index) => (
                         <button
                             key={tab.value}
-                            className={`px-8 pb-16px-plus-2px pt-3 focus:outline-none transition-colors duration-300 ease-in-out ${
+                            className={`px-5 md:px-6 lg:px-8 pb-16px-plus-2px pt-3 focus:outline-none transition-colors duration-300 ease-in-out ${
                                 activeTab === tab.value
                                     ? `border-b-2 ${borderActive} ${textColor}`
                                     : `${textColorInactive} ${hoverButton} hover:border-b-2 ${hoverBorder}`
