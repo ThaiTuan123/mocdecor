5ed7040ce6f128f52a6ebd47d4eebaadc1e94ea9
ThaiTuan <tuanvowork99@gmail.com>
2024-11-09 03:35:54 +0700

    Fix bug các dòng sản phẩm call api sử dụng useState sang sử dụng recoil

--- src/recoil/hooks/useFetchCategories.ts
+++ src/recoil/hooks/useFetchCategories.ts
@@ -1,36 +1,41 @@
-import { useState, useEffect } from 'react';
-import {fetchProductCategories} from "@/services/api";
-import {CategoryType} from "@/types/categoryType";
+// src/hooks/useCategories.ts
+import { useRecoilState } from 'recoil';
+import { fetchProductCategories } from '@/services/api';
+import { useEffect } from 'react';
+import { categoriesState } from "@/recoil/atoms/categoriesAtom";
+import { categoryErrorState } from "@/recoil/atoms/categoryErrorAtom";
 
-const useFetchCategories = (categoryNames: string[]) => {
-    const [categories, setCategories] = useState<CategoryType[]>([]);
-    const [error, setError] = useState<Error | null>(null);
+const useCategories = (categoryNames: string[]) => {
+    const [categories, setCategories] = useRecoilState(categoriesState);
+    const [, setError] = useRecoilState(categoryErrorState); // Accessing the error atom
 
     useEffect(() => {
         const fetchCategories = async () => {
-            try {
-                const results = await Promise.all(
-                    categoryNames.map(async (name) => {
-                        const response = await fetchProductCategories(name);
-                        return response;
-                    })
-                );
+            if (categories.length === 0) { // Only fetch if not already loaded
+                try {
+                    const results = await Promise.all(
+                        categoryNames.map(async (name) => {
+                            const response = await fetchProductCategories(name);
+                            return response;
+                        })
+                    );
 
-                const combinedResults = results.flatMap((res) =>
-                    res && res.items ? res.items : []
-                );
+                    const combinedResults = results.flatMap((res) =>
+                        res && res.items ? res.items : []
+                    );
 
-                setCategories(combinedResults);
-            } catch (err) {
-                console.error("Error fetching categories:", err);
-                setError(err instanceof Error ? err : new Error("Unknown error occurred"));
+                    setCategories(combinedResults);
+                } catch (err) {
+                    console.error("Error fetching categories:", err);
+                    setError(err instanceof Error ? err : new Error("Unknown error occurred"));
+                }
             }
         };
 
         fetchCategories();
-    }, [categoryNames]);
+    }, [categoryNames, categories, setCategories, setError]);
 
-    return { categories, error };
+    return { categories };
 };
 
-export default useFetchCategories;
+export default useCategories;
