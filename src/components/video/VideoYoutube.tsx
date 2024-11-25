ff8092f6b4e75e2ebe2b154a9614ed8733fd3619
Thai Tuan <53283080+ThaiTuan123@users.noreply.github.com>
2024-11-26 04:46:43 +0700

    optimize video code

--- src/components/video/VideoYoutube.tsx
+++ src/components/video/VideoYoutube.tsx
@@ -1,6 +1,6 @@
 // components/VideoYoutube.tsx
-import React from 'react';
-import {getIframeSrc} from "@/utils/videoConstants";
+import React, { useEffect, useState } from 'react';
+import { getIframeSrc } from "@/utils/videoConstants";
 
 interface VideoProps {
     videoId: string;
@@ -19,18 +19,32 @@ const VideoYoutube: React.FC<VideoProps> = ({
                                                 loop = true,
                                                 height = '402px', // Default height
                                             }) => {
-    const iframeSrc = getIframeSrc(videoId, autoplay, mute, controls, loop);
+    const [iframeSrc, setIframeSrc] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchIframeSrc = async () => {
+            const src = await getIframeSrc(videoId, autoplay, mute, controls, loop);
+            setIframeSrc(src);
+        };
+
+        fetchIframeSrc();
+    }, [videoId, autoplay, mute, controls, loop]);
+
+    if (!iframeSrc) {
+        return <div>Loading video...</div>; // Hoặc loading spinner tùy bạn
+    }
 
     return (
-        <div className="relative w-full" style={{height, position: 'relative', paddingBottom: '56.25%'}}>
+        <div className="relative w-full" style={{ height, position: 'relative', paddingBottom: '56.25%' }}>
             <iframe
                 src={iframeSrc}
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowFullScreen
+                loading="lazy"
                 className="absolute top-0 left-0 w-full h-full"
-                style={{pointerEvents: 'none'}} // Disables interaction
+                style={{ pointerEvents: 'none' }} // Disables interaction
             />
         </div>
     );
