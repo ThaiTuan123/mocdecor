73aecbb111253f078e683aafac2b631a2db6af13
khanhnn <khanhnn@dipro.vn>
2024-11-26 23:58:43 +0700

    fix: build prod parallel

--- src/app/gallery/layout.tsx
+++ src/app/gallery/layout.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import FooterDiscover from "@/components/footer/FooterDiscover";
 
 interface LayoutProps {
-    children: React.ReactNode;
-    Empty?: React.ReactNode; // Changed to ReactNode
-    FoundGallery?: React.ReactNode; // Changed to ReactNode
-    Notfound?: React.ReactNode; // Changed to ReactNode
+  children: React.ReactNode;
+  Empty: React.ReactNode;
+  FoundGallery: React.ReactNode;
+  Notfound: React.ReactNode;
 }
 
-export default function Layout({children}: LayoutProps) {
-    return (
-        <>
-            {children}
-            <FooterDiscover/>
-        </>
-    );
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      {children}
+      <FooterDiscover />
+    </>
+  );
 }
