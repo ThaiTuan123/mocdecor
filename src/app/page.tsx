ed009e5546ef6a3b3764f419eaab7391a44de856
Thai Tuan <53283080+ThaiTuan123@users.noreply.github.com>
2024-12-21 17:05:51 +0700

    change image git

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,13 +37,13 @@ export const metadata: Metadata = {
     title: 'Mộc Decor | Trang chủ - Nơi lưu giữ yêu thương và kỉ niệm của bạn',
     description:
       'Mộc Decor chuyên in ảnh chất lượng cao và thủ công mỹ nghệ tinh xảo, cung cấp giải pháp trang trí độc đáo và cá nhân hóa cho ngôi nhà và doanh nghiệp của bạn.',
-    url: 'https://mocdecor.com',
+    url: 'https://raw.githubusercontent.com/ThaiTuan123/mocdecor/refs/heads/main/public/assets/imgs/pictures/og-image.png',
     siteName: 'Mộc Decor',
     type: 'website',
     locale: 'vi_VN',
     images: [
       {
-        url: 'https://mocdecor.vercel.app/imgs/pictures/og-image.png',
+        url: 'https://raw.githubusercontent.com/ThaiTuan123/mocdecor/refs/heads/main/public/assets/imgs/pictures/og-image.png',
         width: 1200,
         height: 630,
         alt: 'Mộc Decor - Nơi lưu giữ yêu thương và kỉ niệm của bạn',
@@ -57,7 +57,9 @@ export const metadata: Metadata = {
     title: 'Mộc Decor | Trang chủ - Ảnh In, Khung Ảnh, Album Ảnh',
     description:
       'Mộc Decor chuyên in ảnh chất lượng cao và thủ công mỹ nghệ tinh xảo, cung cấp giải pháp trang trí độc đáo và cá nhân hóa cho ngôi nhà và doanh nghiệp của bạn.',
-    images: ['https://mocdecor.vercel.app/imgs/pictures/og-image.png'],
+    images: [
+      'https://raw.githubusercontent.com/ThaiTuan123/mocdecor/refs/heads/main/public/assets/imgs/pictures/og-image.png',
+    ],
   },
   alternates: {
     canonical: 'https://mocdecor.com',
