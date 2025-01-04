68eb05694c16b475a86ffb0ddb2821bb9cddbc7b
Thai Tuan <53283080+ThaiTuan123@users.noreply.github.com>
2025-01-05 01:54:21 +0700

    fix square product line

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -26,7 +26,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ onClick, ...product }) => {
       className="transform cursor-pointer rounded bg-white p-1 ring-1 ring-stroke transition-transform duration-300 ease-in-out hover:outline-4 hover:ring-caption md:p-4"
       onClick={onClick}
     >
-      <div className="relative h-44 w-full md:h-52 lg:h-220 xl:h-238">
+      <div className="aspect-h-1 aspect-w-1 relative w-full">
         {isLoading ? (
           // Skeleton Loader for Image
           <div className="absolute inset-0 animate-pulse rounded bg-gray-200"></div>
