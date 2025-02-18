3299c86d73ae4b46fe8a259c1e047d27f5469e81
khanhnn <khanh.ngo.2303@gmail.com>
2025-02-18 17:49:27 +0700

    fix: add domain get image from server

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,10 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'cdn.mocdecor.org',
+      },
+      {
+        protocol: 'https',
         hostname: 'picsum.photos',
       },
       {
